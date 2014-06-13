620a0ef00afb6b0c795b1fdfa408f95e5c5b7d61
VioletaP <popova.vili@gmail.com>
2014-06-13 21:43:59 +0300

    initial game file
    human' choice, computer' random choice, .inspect - shows data type

---
+++ rockpaperscissors.rb
@@ -0,0 +1,9 @@
+puts "Hello my friend. Let's play Rock, Paper, Scissors for fun"
+puts "Insert your choice: R, P, S"
+human=gets
+puts human.inspect
+
+computer = [:R, :S, :P].sample
+puts computer.inspect
+
+
