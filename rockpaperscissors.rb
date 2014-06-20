dd80e24924f6706ac90985429ab31d4305eeb09f
VioletaP <popova.vili@gmail.com>
2014-06-20 21:48:14 +0300

    first working version

--- rockpaperscissors.rb
+++ rockpaperscissors.rb
@@ -1,9 +1,19 @@
 puts "Hello my friend. Let's play Rock, Paper, Scissors for fun"
 puts "Insert your choice: R, P, S"
-human=gets
-puts human.inspect
+human=gets.chomp
+puts "Your choice is #{human}"
 
-computer = [:R, :S, :P].sample
-puts computer.inspect
+computer = ["R", "S", "P"].sample
+puts "My choice is #{computer}"
 
-
+if human==computer
+  puts "nobody wins"
+elsif human == "R" && computer == "P"
+  puts "looser"
+elsif human == "P" && computer == "S"
+  puts "looser"
+elsif human == "S" && computer == "R"
+  puts "looser"
+else
+  puts "bravoooo"
+end
